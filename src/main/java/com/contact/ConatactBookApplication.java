af2645e2a7525981ed8458a8acbc6f2b8dcfd1f8
Shubham Dhadse <shubhamdhadse0827@gmail.com>
2026-06-17 23:59:13 +0530

    update ContactBookApplication and application properties

--- src/main/java/com/contact/ConatactBookApplication.java
+++ src/main/java/com/contact/ConatactBookApplication.java
@@ -3,6 +3,8 @@ package com.contact;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import jakarta.annotation.PostConstruct;
+
 @SpringBootApplication
 public class ConatactBookApplication {
 
@@ -10,4 +12,12 @@ public class ConatactBookApplication {
 		SpringApplication.run(ConatactBookApplication.class, args);
 	}
 
+	@PostConstruct
+	public void test() {
+	    System.out.println("MAIL_USERNAME = "
+	            + System.getenv("MAIL_USERNAME"));
+
+	    System.out.println("MAIL_PASSWORD EXISTS = "
+	            + (System.getenv("MAIL_PASSWORD") != null));
+	}
 }
